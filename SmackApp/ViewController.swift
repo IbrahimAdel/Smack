78b3f7235e7a01c92be9e419febd5dd106cd38ad
Ibrahim Hussein <ibrahim@ibrahims-macbook-pro.local>
2018-04-03 18:01:09 +0300

    Initial Commit

---
+++ SmackApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmackApp
+//
+//  Created by Ibrahim Hussein on 03/04/2018.
+//  Copyright © 2018 Ibrahim Hussein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
