ab1516ae166edb29d5bb0b2cdd1d75b13dad476a
Ibrahim Hussein <ibrahim@Ibrahims-MacBook-Pro.local>
2018-04-03 22:59:32 +0300

    end of lesson 2

---
+++ SmackApp/Controller/ChatVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChatVC.swift
+//  SmackApp
+//  Copyright © 2018 Ibrahim Hussein. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    //outlets
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+}
