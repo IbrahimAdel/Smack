4f8118edcade95fd72994f9b3959b362d1f4403d
Ibrahim Hussein <ibrahim@Ibrahims-MacBook-Pro.local>
2018-04-04 04:09:10 +0300

    auth for reigster user

--- SmackApp/Utilities/Constants.swift
+++ SmackApp/Utilities/Constants.swift
@@ -7,9 +7,22 @@
 //
 
 import Foundation
+typealias completionHandler = (_ Success:Bool) -> ()
+
+//URL
+
+let BASE_URL = "https://chatapp1999.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
 
 //Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannelVC"
+
+
+//User Defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "logged in"
+let USER_EMAIL = "userEmail"
