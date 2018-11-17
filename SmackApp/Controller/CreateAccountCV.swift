b90c7c51e98bfdd16178e8cf6be3473b96729e84
IbrahimAdel <38044785+IbrahimAdel@users.noreply.github.com>
2018-11-17 17:45:23 +0300

    UserData implemented

--- SmackApp/Controller/CreateAccountCV.swift
+++ SmackApp/Controller/CreateAccountCV.swift
@@ -6,8 +6,16 @@ import UIKit
 
 class CreateAccountCV: UIViewController {
 
+    //outlets
+    @IBOutlet weak var usernameTXT: UITextField!
     @IBOutlet weak var emailTXT: UITextField!
     @IBOutlet weak var passTXT: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    //variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,15 +25,22 @@ class CreateAccountCV: UIViewController {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
     @IBAction func createAccountPressed(_ sender: Any) {
+        guard let name = usernameTXT.text, usernameTXT.text != "" else{return}
         guard let email = emailTXT.text, emailTXT.text != "" else{return}
         guard let password = passTXT.text, passTXT.text != "" else{return}
         
         AuthService.instance.registerUser(email: email, password: password) { (success) in
             if(success){
-                print("Registered User!")
-                AuthService.instance.loginUser(email: email, password: password, completion: { (success2) in
-                    if (success2){
-                        print("Logged in User!")
+                print("registered")
+                AuthService.instance.loginUser(email: email, password: password, completion: { (success) in
+                    if (success){
+                        print("logged in")
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print("Id: \(UserDataService.instance.id)")
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
---
+++ SmackApp/Services/UserDataService.swift
@@ -0,0 +1,31 @@
+//
+//  UserData.swift
+//  SmackApp
+//
+//  Created by Ibrahim Hussein on 17/11/2018.
+//  Copyright © 2018 Ibrahim Hussein. All rights reserved.
+//
+
+import Foundation
+
+class UserDataService {
+    
+    static let instance = UserDataService()
+    public private(set) var id = ""
+    public private(set) var avatarColor = ""
+    public private(set) var avatarName = ""
+    public private(set) var email = ""
+    public private(set) var name = ""
+    
+    func setUserData(id: String, color: String, avatarName: String, email: String, name: String){
+        self.id = id
+        self.avatarColor = color
+        self.avatarName = name
+        self.email = email
+        self.name = name
+    }
+    
+    func setAvatarName(avatarName: String) {
+        self.avatarName = avatarName
+    }
+}
